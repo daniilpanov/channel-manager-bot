2cd85ae0cb7c9b4fb6c1d7f340bd305b23b15f0e
Daniil Panov <daniil_panov2005@mail.ru>
2023-01-15 02:33:53 +0300

    hotfix: DB

--- DB.py
+++ DB.py
@@ -3,46 +3,49 @@ from ENV import env
 
 
 class DB:
+    _config = None
+    _connection = None
+    _cursor = None
+    _dict_cursor = None
+    prefix = None
+
     def __init__(self, host, user, password, database, port='3306', prefix=None):
-        try:
-            self.config = {
-                'host': host,
-                'user': user,
-                'password': password,
-                'database': database,
-                'port': port,
-                'prefix': prefix,
-            }
-            self.connection = connect(
-                host=host,
-                user=user,
-                password=password,
-                database=database,
-                port=port,
-            )
-            self.success = True
+        self._config = {
+            'host': host,
+            'user': user,
+            'password': password,
+            'database': database,
+            'port': port,
+            'autocommit': True,
+        }
+        if self.__connect():
             self.prefix = prefix
-        except Error as e:
-            print(e.msg)
-            self.connection = None
-            self.success = False
 
-    def query(self, sql, params=None, prefix=True, retry=True):
+    def __connect(self):
+        try:
+            self._connection = connect(**self._config)
+            self._cursor = self._connection.cursor()
+            self._dict_cursor = self._connection.cursor(dictionary=True)
+            return True
+        except Error:
+            self._connection = None
+            return False
+
+    def query(self, sql, params=None, prefix=True, simple=False, retry=True):
         if prefix:
-            sql = add_prefix(sql)
+            sql = sql.replace('alliances', self.prefix + 'alliances') \
+                .replace('tasks', self.prefix + 'tasks') \
+                .replace('users', self.prefix + 'users')
 
         try:
-            cursor = self.connection.cursor()
+            cursor = self._cursor if simple else self._dict_cursor
             cursor.execute(sql, params)
-            res = cursor.fetchall()
-            self.connection.commit()
-            return res
+            return cursor.fetchall()
         except Error as e:
-            print(e.msg)
-            if retry:
-                new_inst = reconnect(DB(**self.config))
-                if new_inst.success:
-                    return new_inst.query(sql, params, False)
+            print('Error occurred in MySQL', e.msg, 'Try to reconnect')
+            if retry and self.__connect():
+                return self.query(sql, params, False, simple, False)
+            print('Reconnection failed! End sql and params: ', end='')
             print(sql)
             print(params)
             return False
@@ -56,15 +59,3 @@ db = DB(
     env.get('db_port'),
     env.get('db_prefix'),
 )
-
-
-def add_prefix(sql):
-    return sql.replace('alliances', db.prefix + 'alliances') \
-        .replace('tasks', db.prefix + 'tasks') \
-        .replace('users', db.prefix + 'users')
-
-
-def reconnect(inst):
-    global db
-    db = inst
-    return inst
